836ecbb47a30e26d29c1db133bc267209d38f028
salonikumari12 <162488452+salonikumari12@users.noreply.github.com>
2025-09-13 11:54:41 +0530

    Update Button.js

--- src/Styled/Button.js
+++ src/Styled/Button.js
@@ -1,1 +1,40 @@
+import styled from "styled-components"
+ 
 
+export  const Button = styled.button`
+  color: white;
+  padding: 10px 18px;
+
+  background:black;
+  border-radius: 5px;
+  min-width: 220px;
+  border: none;
+
+  font-size: 16px; 
+
+  border: 1px solid transparent;
+  transition: background-color 0.4s ease 0s;
+
+  &:hover{
+    background-color: white;
+    border: 1px solid black;
+    color:black;
+    transition: background-color 0.3s ease 0s;
+  }
+
+`;
+
+
+export const OutlineButton = styled(Button)`
+background-color: white;
+
+ border: 1px solid black;
+ color: black;
+ 
+  &:hover{
+    background-color: black;
+    border: 1px solid black;
+    color:white;
+    
+  }
+`;
